e4e96bcc8b376ad9a6ae064a0cac926c3605c899
Qi Li <liqithu@gmail.com>
2019-11-25 13:00:52 -0600

    Add post-process

--- parametric_analysis/param_run.py
+++ parametric_analysis/param_run.py
@@ -11,6 +11,7 @@ import pathlib
 import shutil
 import json
 import csv
+import sqlite3
 
 import numpy as np
 import pandas as pd
@@ -21,7 +22,6 @@ from add_measure import *
 def perturb_model(
         osm_file, epw_file, design_file, config_file, measure_file, run=None):
     """"""
-    run = 'osm' if run is None else run
 
     # Provide OpenStudio path and current file path
     with open(config_file, 'r') as f:
@@ -50,12 +50,20 @@ def perturb_model(
     # Directory for analysis outputs
     out_dir = root_dir.joinpath('output')
 
+    # Directory for visualization plots
+    plot_dir = root_dir.joinpath('plot')
+
     # Create directories
-    for dir_inst in [osw_dir, mdl_dir, run_dir, out_dir]:
-        shutil.rmtree(dir_inst, ignore_errors=True)
-        pathlib.Path.mkdir(dir_inst)
+    for dir_inst in [osw_dir, mdl_dir, run_dir, out_dir, plot_dir]:
+        try:
+            pathlib.Path.mkdir(dir_inst)
+        except FileExistsError:
+            continue
     for mdl in ['idf', 'osm']:
-        pathlib.Path.mkdir(mdl_dir.joinpath(mdl))
+        try:
+            pathlib.Path.mkdir(mdl_dir.joinpath(mdl))
+        except FileExistsError:
+            continue
 
     # Create osw template and model params in JSON
     osw_template = {}
@@ -74,8 +82,6 @@ def perturb_model(
     for i in range(lookup_table.shape[0]):
         measure_list.append(lookup_table.loc[i, :].to_dict())
 
-    # Set RUBYLIB environment variable RUBYLIB
-
     # Create osw file using OpenStudio
     n_run = design_table.shape[0]
     osw_path = []
@@ -84,7 +90,7 @@ def perturb_model(
         osw_inst = osw_template.copy()
 
         # Run directory
-        run_inst_dir = run_dir.joinpath('run-inst_{}'.format(i+1))
+        run_inst_dir = run_dir.joinpath('{}_Inst_{}'.format(mdl_name, i+1))
         run_inst_dirs.append(run_inst_dir)
         osw_inst['run_directory'] = str(run_inst_dir)
 
@@ -94,6 +100,17 @@ def perturb_model(
             osw_inst['steps'].extend(
                 create_measure(measure, design_table.iloc[i, j])
             )
+
+        # TEMPORARY: Add outside air temperature output
+        osw_inst['steps'].append(
+            {
+                'measure_dir_name': 'AddOutputVariable',
+                'arguments': {
+                    'variable_name': 'Site Outdoor Air Drybulb Temperature',
+                    'reporting_frequency': 'timestep'
+                }
+            }
+        )
 
         # Generate osw files
         osw_path_inst = osw_dir.joinpath(
@@ -107,7 +124,9 @@ def perturb_model(
     if run == 'osm':
         # OpenStudio workflow
         for i in range(n_run):
-            command_line = '{} run -w "{}"'.format(exe_path, str(osw_path[i]))
+            command_line = '{} run -w "{}"'.format(
+                exe_path, str(osw_path[i])
+            )
             os.system(command_line)
 
             # Copy the in.idf and in.osm to the model folder
@@ -118,7 +137,7 @@ def perturb_model(
                     file_name, file_ext
                 )
 
-    else:
+    else if run == 'idf':
         # EnergyPlus workflow
         for i in range(n_run):
             command_line = '{} run -m -w "{}"'.format(
@@ -134,6 +153,35 @@ def perturb_model(
                     file_name, file_ext
                 )
         pass
+
+    else:
+        # Post-process only
+        pass
+
+    # TEMPORARY: Collect simulation outputs
+    rng = pd.date_range('1/1/2017 0:00', '12/31/2017 23:45', freq='15min')
+    out_spec = {
+        'Environment:Site Outdoor Air Drybulb Temperature [C](TimeStep)': (
+            'SELECT Value FROM ReportVariableWithTime '
+            'WHERE KeyValue="Environment" '
+            'AND Name="Site Outdoor Air Drybulb Temperature" '
+            'AND ReportingFrequency="Zone Timestep"'
+        ),
+        'Electricity:Facility [J](TimeStep)': (
+            'SELECT Value FROM ReportVariableWithTime '
+            'WHERE Name="Electricity:Facility" '
+            'AND ReportingFrequency="Zone Timestep"'
+        )
+    }
+    for d in run_dir.iterdir():
+        if d.is_dir():
+            out_file = d.joinpath('eplusout.sql')
+            rlt_file = out_dir.joinpath('{}.csv'.format(d.stem))
+            with sqlite3.connect(out_file) as conn:
+                df = pd.DataFrame(index=rng)
+                for name, query in out_spec.items():
+                    df[name] = pd.read_sql_query(query, conn).values
+            df.to_csv(rlt_file)
 
 
 def copy_model_file(src_dir, dst_dir, file_name, file_ext=None):
